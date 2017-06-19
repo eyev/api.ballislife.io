00f0cb86908bba819ba49f57a6d85e797c36ffae
Justin Conklin <justin@eyev.org>
2017-06-18 21:27:53 -0400

    fix(server): add CORS to express instance

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,26 @@
+import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
-import { ApplicationModule } from './modules/app.module';
+import { ApplicationModule } from './app.module';
 
-const app = NestFactory.create(ApplicationModule);
-app.listen(3000, () => console.log('Application is listening on port 3000.'));
+const instance = express();
+instance.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+const app = NestFactory.create(ApplicationModule, instance);
+app.listen(3000, () => console.log('Application is listening on port 3000'));
