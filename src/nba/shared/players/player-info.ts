766c3c094f40229bd0f304920a7e8acf9867544d
Justin Conklin <justin@eyev.org>
2017-06-18 21:29:29 -0400

    feat(shared): add shared types and exported static variables (teams, players)

---
+++ src/nba/shared/players/player-info.ts
@@ -0,0 +1,15 @@
+export class PlayerInfo { 
+    person_id: number;
+    display_last_comma_first: string;
+    display_first_last: string;
+    rosterstatus: number;
+    from_year: string;
+    to_year: string;
+    playercode: string;
+    team_id: number;
+    team_city: string;
+    team_name: string;
+    team_abbreviation: string;
+    team_code: string;
+    games_played_flag: string;
+}
---
+++ src/nba/shared/team/team.ts
@@ -0,0 +1,12 @@
+export class Team { 
+    isNBAFranchise: boolean;
+    city: string;
+    altCityName: string;
+    fullName: string;
+    triCode: string;
+    teamId: string;
+    nickname: string;
+    urlName: string;
+    confName: string;
+    divName: string;
+}
