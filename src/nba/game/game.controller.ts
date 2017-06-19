9e768d1781bf47ca17be93bb34a73791b738ea15
Justin Conklin <justin@eyev.org>
2017-06-18 21:29:55 -0400

    feat(gameController): add controller for game endpoints, add factory to reformat nba.js data and return to controller

---
+++ src/nba/game/game.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Post, HttpStatus } from '@nestjs/common';
+
+import { data } from 'nba.js';
+
+import { Game } from './game';
+import { GameFactory } from './game.factory';
+import { GamePreview } from './game-preview';
+
+@Controller('game')
+export class GameController {
+    constructor(private gameFactory: GameFactory) { }
+
+    @Get()
+    getGamesByDate(req, res, next) {
+        data.calendar()
+            .then(calendar => { 
+                res.status(HttpStatus.OK).json(calendar);
+            })
+    }
+
+    @Get('/detail/:date/:gameId')
+    getGameDetail(req, res, next): void {
+        data.boxscore({
+            date: req.params.date,
+            gameId: req.params.gameId
+        }).then(gameDetail => { 
+            res.status(HttpStatus.OK)
+                .json(<Game[]>this.gameFactory.createGameDetail(gameDetail));
+        }).catch(error => {
+            res.status(HttpStatus.NO_CONTENT)
+          });
+    }
+    @Get('/preview/:date')
+    getGamePreview(req, res, next): void {
+        data.scoreboard({
+            date: req.params.date
+        }).then(gamePreview => {
+            console.log('aye?');
+            res.status(HttpStatus.OK)
+                .json(this.gameFactory.createGamePreview(gamePreview))
+        }).catch(error => { 
+            res.status(HttpStatus.NO_CONTENT)
+        })
+        // return new GamePreview();
+    }
+}
---
+++ src/nba/game/game.factory.ts
@@ -0,0 +1,82 @@
+import { Component } from '@nestjs/common';
+
+import * as _ from "lodash";
+import * as moment from 'moment-timezone';
+
+import { GamePlayer } from '../game/game-player';
+import { Game } from '../game/game';
+import { GamePreview } from '../game/game-preview';
+import { PlayerInfo } from '../shared/players/player-info';
+
+import TEAMS from '../shared/team/teams';
+import PLAYERS from '../shared/players/players';
+
+@Component()
+export class GameFactory {
+    /* GameFactory
+          Take the raw returns from NBA api and format for ballislife.io
+    */
+
+    // /game/detail/:date/:gameId
+    createGameDetail(dirtyGame): Array<Game> {
+        let prettyGame = [];
+        let game = {
+            id: dirtyGame['basicGameData'].gameId,
+            isGameStarted: this.isGameStarted(dirtyGame['basicGameData'].startTimeUTC, dirtyGame['basicGameData'].hTeam.linescore.length),
+            clock: dirtyGame['basicGameData'].clock,
+            buzzer: dirtyGame['basicGameData'].isBuzzerBeater,
+            time: moment(dirtyGame['basicGameData'].startTimeUTC, "America/New_York"),
+            period: dirtyGame['basicGameData'].period,
+            home: dirtyGame['basicGameData'].hTeam,
+            visitor: dirtyGame['basicGameData'].vTeam,
+            broadcast: dirtyGame['basicGameData'].watch.broadcast.broadcasters.national
+        }
+        game.home.stats = dirtyGame['stats'].hTeam;
+        game.visitor.stats = dirtyGame['stats'].vTeam;
+        game.home.players = this.getPlayers(game.home.teamId, dirtyGame['stats'].activePlayers);
+        game.visitor.players = this.getPlayers(game.visitor.teamId, dirtyGame['stats'].activePlayers);
+        game.home.info = _.find(TEAMS, teamInfo => teamInfo.teamId == game.home.teamId);
+        game.visitor.info = _.find(TEAMS, teamInfo => teamInfo.teamId == game.visitor.teamId);
+
+        prettyGame.push(game);
+        return prettyGame;
+    }
+
+    // /game/preview/:date
+    createGamePreview(dirtyGames): GamePreview[] {
+        console.log(dirtyGames);
+        let games = dirtyGames.games.map(game => {
+            return {
+                id: game.gameId,
+                isGameStarted: this.isGameStarted(game.startTimeUTC, game.hTeam.linescore.length),
+                clock: game.clock,
+                buzzer: game.isBuzzerBeater,
+                time: moment.tz(game.startTimeUTC, "America/New_York"),
+                period: game.period,
+                home: game.hTeam,
+                visitor: game.vTeam,
+                broadcast: game.watch.broadcast.broadcasters.national
+            }
+        });
+        return games;
+
+    }
+    // sifts through response and gets players for a specific team
+    private getPlayers(teamId, activePlayers) {
+        let players = _.filter(activePlayers, player => player['teamId'] === teamId);
+        // cycle through each player and add their player info
+        players.forEach(player => {
+            player['playerInfo'] = this.getPlayerInfo(player['personId']);
+        });
+
+        return players;
+    }
+
+    private getPlayerInfo(playerId): PlayerInfo {
+        return _.filter(PLAYERS, (theplayer) => theplayer['person_id'] === +playerId)[0];
+    }
+
+    private isGameStarted(gameTime: string, score: number): boolean {
+        return moment().utc().isAfter(gameTime, 'minute') && score > 0;
+    }
+}
